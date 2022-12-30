360a22a9d9314364e83037afec8841266003b6e3
tom1299 <tom1299@jeeatwork.com>
2022-12-30 14:29:28 +0100

    Add some more comments

--- baisc-network-1.py
+++ baisc-network-1.py
@@ -19,22 +19,26 @@ def generate_test_data():
 def create_model():
     model = tf.keras.Sequential([tf.keras.layers.Dense(units=1, input_shape=[2])])
     model.compile(optimizer='adam', loss='mean_squared_error')
+    model.summary()
     return model
 
 
 model = create_model()
 
 xs, ys, zs = generate_test_data()
-print(f"xs: {xs.shape}, ys: {ys.shape}, zs: {zs.shape}")
 
 # xs and ys are 1D arrays, so we need to stack them to create a 2D array
 # with shape (10000, 2) where each row is a pair of values
 # from xs and ys and is compatible with the input_shape of the model
 # which is (None,2) where None is the number of rows in the input
+# and 2 is the number of columns in the input which is the number of
+# features in the input or in this case the number of parameters of the
+
 xs_ys_in2d = np.vstack((xs, ys)).T
+print(f"xs: {xs.shape}, ys: {ys.shape}, zs: {zs.shape}, xyin2d: {xs_ys_in2d.shape} model: {model.input_shape}")
 
 history = model.fit(xs_ys_in2d, zs, epochs=100, callbacks=[StopOnLoss()], validation_split=0.2,
-                    shuffle=True)
+                    shuffle=True, verbose=0)
 
 print(model.predict([[2.0, 3.0]]))
 print(secret_function(2.0, 3.0))
