b165826edc0fba7cb66035e06dadb43f55e0a500
tom1299 <tom1299@jeeatwork.com>
2022-12-30 12:13:22 +0100

    Made a first example with a very basic network

---
+++ baisc-network-1.py
@@ -0,0 +1,43 @@
+import numpy as np
+import tensorflow as tf
+
+import ml_utils
+from ml_utils import StopOnLoss
+
+def secret_function(x, y):
+    z = x + y
+    return z
+
+
+def generate_test_data():
+    xs = np.arange(-5000, 5000, 1, dtype=np.float32)
+    ys = np.arange(-5000, 5000, 1, dtype=np.float32)
+    zs = secret_function(xs, ys)
+    return xs, ys, zs
+
+
+def create_model():
+    model = tf.keras.Sequential([tf.keras.layers.Dense(units=1, input_shape=[2])])
+    model.compile(optimizer='adam', loss='mean_squared_error')
+    return model
+
+
+model = create_model()
+
+xs, ys, zs = generate_test_data()
+print(f"xs: {xs.shape}, ys: {ys.shape}, zs: {zs.shape}")
+
+# xs and ys are 1D arrays, so we need to stack them to create a 2D array
+# with shape (10000, 2) where each row is a pair of values
+# from xs and ys and is compatible with the input_shape of the model
+# which is (None,2) where None is the number of rows in the input
+xs_ys_in2d = np.vstack((xs, ys)).T
+
+history = model.fit(xs_ys_in2d, zs, epochs=100, callbacks=[StopOnLoss()], validation_split=0.2,
+                    shuffle=True)
+
+print(model.predict([[2.0, 3.0]]))
+print(secret_function(2.0, 3.0))
+
+print(history.history.keys())
+ml_utils.plot_loss(history)
---
+++ ml_utils.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+class StopOnLoss(tf.keras.callbacks.Callback):
+
+    def __init__(self, loss=1.0e-05):
+        self.__loss = loss
+
+    def on_epoch_end(self, epoch, logs=None):
+        if logs is None:
+            logs = {}
+        if logs.get('loss') <= self.__loss:
+            print(f"Reached {self.__loss} loss")
+            self.model.stop_training = True
+
+def plot_loss(history):
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    epochs = range(len(loss))
+    plt.plot(epochs, loss, 'r', label='Training loss')
+    plt.plot(epochs, val_loss, 'b', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.legend(loc=0)
+    plt.figure()
+    plt.show()
